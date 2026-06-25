5b26626d5052f94da41bb4ebb5157f4bcd9a4369
Aditya Goyal <learnify5500@gmail.com>
2026-06-25 14:36:52 +0530

    feat: initialize luxury frontend architecture

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+function Home() {
+  return (
+    <div>
+      <h1>
+        GAJAANAND GROUP
+      </h1>
+
+      <p>
+        BUILDING TRUST.
+        CREATING VALUE.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
