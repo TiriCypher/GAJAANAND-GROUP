09ce81724ece3eb83e090bee5b7a54055e6601ca
Aditya Goyal <learnify5500@gmail.com>
2026-06-26 18:30:00 +0530

    feat: setup redux authentication architecture

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+
+import { store } from "./app/store";
 
 import "./styles/globals.css";
 import App from "./App";
@@ -9,8 +12,10 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>
 );
